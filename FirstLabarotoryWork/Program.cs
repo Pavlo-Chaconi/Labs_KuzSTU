f7311637ea3fa7e3de402d73ec09117e21d5e73c
user <user@USER->
2024-05-16 13:38:39 +0700

    Обновление

--- FirstLabarotoryWork/Program.cs
+++ FirstLabarotoryWork/Program.cs
@@ -10,6 +10,12 @@ namespace FirstLabarotoryWork
     {
         static void Main(string[] args)
         {
+            CraneDefinition craneDefinition_program_class = new CraneDefinition();
+            Console.WriteLine(craneDefinition_program_class.CranePassport() + "\n");
+            CraneDefinition craneDefinition_program_class_another_crane = new CraneDefinition(100, 200, 150);
+            Console.WriteLine(craneDefinition_program_class_another_crane.CranePassport() + "\n");
+            Console.Read();
+            ewde
         }
     }
 }
