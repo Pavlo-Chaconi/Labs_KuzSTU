58a51f49e91e9a5770479edee5edc367831df75e
user <user@USER->
2024-05-16 16:59:16 +0700

    Окончательный пример

---
+++ CurrencyConverter/CurrencyConverterClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyConverter;
+
+namespace CurrencyConverter
+{
+    public static class CurrencyConverterClass
+    {
+        public const double USD_RUB = 61.87;
+        public const double EUR_USD = 70.44;
+        public const double CNY_RUB = 9.89;
+        public static double response;
+
+        public static double convertRUBtoUSD(double RUB)
+        {
+            return response = RUB / USD_RUB;
+        }
+
+        public static double convertUSDtoEUR(double USD)
+        {
+            return response = (USD_RUB / USD) * EUR_USD;
+        }
+
+        public static double convertCNYtoRUB(double CNY)
+        {
+            return response = CNY * CNY_RUB;
+        }
+    }
+}
--- ThirdLabaratoryProject/Program.cs
+++ ThirdLabaratoryProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CurrencyConverter;
 
 namespace ThirdLabaratoryProject
 {
@@ -10,6 +11,34 @@ namespace ThirdLabaratoryProject
     {
         static void Main(string[] args)
         {
+            do
+            {
+                Console.WriteLine("Для конвертации рублей в доллары нажмите 1 \n" + 
+                    "Для конвертации долларов в евро нажмите 2 \n" + 
+                    "Для конвертации юаней в рубли нажмите 3");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Введите сумму для ковертации в доллары");
+                        double RUBStoUSD = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Конвертировано {0} рублей в {1} долларов", RUBStoUSD, CurrencyConverterClass.convertRUBtoUSD(RUBStoUSD));
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Введите сумму для ковертации в евро");
+                        double USDtoEUR = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Конвертировано {0} долларов в {1} евро", USDtoEUR, CurrencyConverterClass.convertUSDtoEUR(USDtoEUR));
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите сумму для ковертации в рубли");
+                        double CNYtoRUB = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Конвертировано {0} юаней в {1} рубли", CNYtoRUB, CurrencyConverterClass.convertCNYtoRUB(CNYtoRUB));
+                        break;
+
+                }
+            }
+            while (true);
+
         }
     }
 }
